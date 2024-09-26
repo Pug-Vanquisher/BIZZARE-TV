9a711ddd78fa2fa9755b609d16a91135deb4bbf6
Roman784 <rmironenko50@gmail.com>
2024-09-26 12:25:50 +0300

    Массив DI инсталлеров вместо одного

--- BizarreTV/Assets/Balance/Develop/Architecture/EntryPoints/EntryPoint.cs
+++ BizarreTV/Assets/Balance/Develop/Architecture/EntryPoints/EntryPoint.cs
@@ -5,10 +5,16 @@ namespace Balance
 {
     public abstract class EntryPoint : MonoBehaviour
     {
-        [SerializeField] private DIInstaller _installer;
+        [SerializeField] private DIInstaller[] _installers;
 
         public abstract IEnumerator Run();
 
-        protected void InstallBindings() => _installer.InstallBindings();
+        protected void InstallBindings()
+        {
+            foreach (DIInstaller installer in _installers)
+            {
+                installer.InstallBindings();
+            }
+        }
     }
 }
