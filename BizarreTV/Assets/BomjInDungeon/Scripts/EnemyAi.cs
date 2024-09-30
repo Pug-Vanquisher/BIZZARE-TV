9f5ad06e4697eae05c17ba60653ccf843782c08c
koldyn-dpo4ula <pengwogun@gmail.com>
2024-09-30 09:52:23 +0300

    oral cumshot

---
+++ BizarreTV/Assets/BomjInDungeon/Scripts/EnemyAi.cs
@@ -0,0 +1,67 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public Vector2 velik;
+
+    private RaycastHit2D hit;
+
+    public float attackRange;
+    public float vlframes;
+    private float current_vlframes;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_vlframes = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) <= attackRange & current_vlframes == 0)
+        {
+            Attack();
+        }
+        else if (current_vlframes == 0)
+        {
+            //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            //velik = player.transform.position - transform.position;
+
+            if (player.transform.position.x >= transform.position.x) { velik.x = 1; }
+            if (player.transform.position.x < transform.position.x) { velik.x = -1; }
+            if (player.transform.position.y >= transform.position.y) { velik.y = 1; }
+            if (player.transform.position.y < transform.position.y) > 0.2) { velik.y = -1; }
+
+            
+
+            hit = Physics2D.BoxCast(transform.position, Vector2.one, 0, velik, Vector2.Distance(transform.position, new Vector2(transform.position.x, transform.position.y) + velik.normalized * speed), LayerMask.GetMask("Actor", "Blocking"));
+
+            if (hit.collider == null) { gameObject.GetComponent<Rigidbody2D>().MovePosition(new Vector2(transform.position.x, transform.position.y) + velik.normalized * speed); }
+
+        }
+        else if (current_vlframes < 0) { current_vlframes = 0; }
+
+        else { current_vlframes -= Time.deltaTime; }
+    }
+
+    void Attack()
+    {
+        Debug.Log("палучи пизды!!!");
+        
+        current_vlframes = vlframes;
+    }
+}
--- BizarreTV/Assets/BomjInDungeon/Scripts/PlayerMover.cs
+++ BizarreTV/Assets/BomjInDungeon/Scripts/PlayerMover.cs
@@ -13,6 +13,8 @@ namespace BID
 
         public float speed = 0.025f;
 
+        private RaycastHit2D hit;
+
         public GameObject dagger;
         public float AttackSpreadness = 0.03f;
         void Start()
@@ -24,6 +26,14 @@ namespace BID
         void Update()
         {
             velik = Vector2.zero;
+
+            //Debug.DrawRay(transform.position,side1cos);
+
+            if (Input.GetKey(key.Up)) { velik.y += 1; }
+            if (Input.GetKey(key.Down)) { velik.y += -1; }
+            if (Input.GetKey(key.Right)) { velik.x += 1; }
+            if (Input.GetKey(key.Left)) { velik.x += -1; }
+
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 0;
             Vector3 side1 = mousePosition - transform.position;
@@ -31,29 +41,32 @@ namespace BID
 
             Vector3 side1cos = Quaternion.AngleAxis(-90, Vector3.forward) * side1;
 
-            Debug.DrawRay(transform.position,side1cos);
-
-            if (Input.GetKey(key.Up)) { velik.y += 1; }
-            if (Input.GetKey(key.Down)) { velik.y += -1; }
-            if (Input.GetKey(key.Right)) { velik.x += 1; }
-            if (Input.GetKey(key.Left)) { velik.x += -1; }
-
-            Debug.DrawLine(transform.position, mousePosition);
-            //print(Vector2.SignedAngle(player.transform.position, mousePosition));
-            //print(player.transform.position);
-
             if (Input.GetKeyDown(key.Attack))
             {
                 var a = Instantiate(dagger, transform); //transform.position + side1cos * Random.Range(-1f, 2f) * 0.03f, Quaternion.identity);
                 a.transform.position = transform.position + side1cos * Random.Range(-1f, 1f) * AttackSpreadness;
                 a.transform.rotation = Quaternion.AngleAxis(-Vector2.SignedAngle(mousePosition - a.transform.position, Vector3.right), Vector3.forward);
-                
+
                 //Quaternion.AngleAxis(-Vector2.SignedAngle(side1, side2) + Random.Range(-5, 5), Vector3.forward)
             }
+        }
 
+        private void FixedUpdate()
+        {
+            
 
+            //Debug.DrawLine(transform.position, mousePosition);
+            //print(Vector2.SignedAngle(player.transform.position, mousePosition));
+            //print(player.transform.position);
+
+            hit = Physics2D.BoxCast(transform.position, Vector2.one, 0, velik, Vector2.Distance(transform.position, new Vector2(transform.position.x, transform.position.y) + velik.normalized * speed), LayerMask.GetMask("Actor", "Blocking"));
+            
+            if (hit.collider == null)
+            {
+                pl_tmp.MovePosition(new Vector2(transform.position.x, transform.position.y) + velik.normalized * speed);
+            }
             //player.Move(velik.normalized * speed);
-            pl_tmp.MovePosition(new Vector2(transform.position.x, transform.position.y) + velik.normalized * speed);
+            
         }
     }
 }
