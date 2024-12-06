2c1675138b31964b62555899dc91811772a91b2e
Miri-Solari <horniret@gmail.com>
2024-12-06 18:00:31 +0300

    PauseMenu

--- BizarreTV/Assets/Jupiter-731/Sctipts/Camera/PAUSEMenuTemp.cs
+++ BizarreTV/Assets/Jupiter-731/Sctipts/Camera/PAUSEMenuTemp.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PAUSEMenuTemp : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
     private bool _isPaused = false;
 
     // Update is called once per frame
@@ -11,11 +12,13 @@ public class PAUSEMenuTemp : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape) && !_isPaused)
         {
+            pauseMenu.SetActive(true);
             _isPaused = true;
             Time.timeScale = 0f;
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && _isPaused)
         {
+            pauseMenu.SetActive(false);
             _isPaused = false;
             Time.timeScale = 1f;
         }
